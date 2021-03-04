f162a770385d5efbbb760038ad134ec43c5d7ad0
af_oleg <oleg.kh81@gmail.com>
2021-03-04 19:14:03 +0200

    fix template - property Provider

--- src/main/java/com/academy/core/test/BaseTest.java
+++ src/main/java/com/academy/core/test/BaseTest.java
@@ -1,46 +1,36 @@
 package com.academy.core.test;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import com.academy.core.util.PropertyProvider;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.*;
 
-import java.io.FileReader;
 import java.lang.reflect.Method;
 import java.util.Arrays;
-import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
-    private static final Logger LOG =  LogManager.getLogger(BaseTest.class);
+    private static final Logger LOG =  LoggerFactory.getLogger(BaseTest.class);
 
-    protected Properties prop;
     protected WebDriver driver;
     protected String baseUrl;
-
-    private void loadProperties() throws Exception {
-        String propertyPath = System.getProperty("cfg");
-        prop = new Properties();
-        prop.load(new FileReader(propertyPath));
-    }
 
     @Parameters({"browser"})
     @BeforeClass(alwaysRun = true)
     public void setUp(@Optional("chrome") String browser) throws Exception {
-        loadProperties();
-
-        baseUrl = prop.getProperty("base.url");
+        baseUrl = PropertyProvider.get("base.url");
 
         switch (browser) {
             case "chrome":
-                System.setProperty("webdriver.chrome.driver", prop.getProperty("driver.chrome"));
+                System.setProperty("webdriver.chrome.driver", PropertyProvider.get("driver.chrome"));
                 driver = new ChromeDriver();
                 break;
 
             case "firefox":
-                System.setProperty("webdriver.gecko.driver", prop.getProperty("driver.firefox"));
+                System.setProperty("webdriver.gecko.driver", PropertyProvider.get("driver.firefox"));
                 driver = new FirefoxDriver();
                 break;
         }
--- src/test/java/com/academy/project/DemoTests.java
+++ src/test/java/com/academy/project/DemoTests.java
@@ -1,8 +1,8 @@
 package com.academy.project;
 
 import com.academy.core.test.BaseTest;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -10,7 +10,7 @@ import org.testng.annotations.Test;
 import static com.academy.project.page.HomePage.startFromHome;
 
 public class DemoTests extends BaseTest {
-    private static final Logger LOG =  LogManager.getLogger(DemoTests.class);
+    private static final Logger LOG =  LoggerFactory.getLogger(DemoTests.class);
 
     @Test(dataProvider = "testDataProvider")
     public void testFeature(String value) {
